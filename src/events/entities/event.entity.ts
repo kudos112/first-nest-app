4fb31d800005586d52193c7fd4c580d7e6cd1ed8
Abdul Quddous <quddoux112@gmail.com>
2023-02-17 12:24:08 +0500

    created a transaction and sync it with recomending a coffee

---
+++ src/events/entities/event.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+@Index(['type', 'name'])
+export class Event {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  type: string;
+
+  @Column()
+  name: string;
+
+  @Column('json')
+  payload: Record<string, any>;
+}
